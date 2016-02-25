d57abbc019043c228ff36e8e32ca3bee7e32619f
Sara Bee <sara.bainbridge@gmail.com>
2016-02-24 21:46:54 -0500

    Add React

---
+++ src/components/Root.js
@@ -0,0 +1,20 @@
+import React, { PropTypes } from 'react';
+
+module.exports = React.createClass({
+  render() {
+    return (
+      <html>
+        <head>
+          <title>Opsee</title>
+          <link rel="stylesheet" type="text/css" href="style.css" />
+        </head>
+
+        <body>
+          <div>
+            Welcome to the filet zone.
+          </div>
+        </body>
+      </html>
+    );
+  }
+});
--- src/entry.js
+++ src/entry.js
@@ -1,2 +1,19 @@
-const f = () => { return 'hi'; };
-console.log(f());
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import Root from './components/Root';
+
+// Client render (optional):
+if (typeof document !== 'undefined') {
+  // Client render code goes here...
+}
+
+/*
+ * This is the render function used by the static site generator plugin.
+ */
+module.exports = function render(locals, callback) {
+  const html = ReactDOMServer.renderToStaticMarkup(
+    <Root assets={locals.assets} />
+  );
+  callback(null, `<!DOCTYPE html>${html}`);
+};
